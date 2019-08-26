efe80bfea503b9f63d9831cb735e0d1fe08dd417
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-08-26 11:34:35 -0700

    added more basic structure

--- front-end/src/components/TripList.js
+++ front-end/src/components/TripList.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Trip from "./Trip";
 
+//add data regarding existing Trips
+const placeData = [{ destination: "Halekala", date: "May2-31" }];
+
 const TripList = () => {
   return (
     <div>
+      <button>Create a New Trip</button>
       <Trip />
     </div>
   );
