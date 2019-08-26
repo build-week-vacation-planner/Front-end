804b81810de3c972aa851a955f30f2eaa9893d6d
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-08-26 10:37:01 -0700

    component setup

--- front-end/src/components/Welcome.js
+++ front-end/src/components/Welcome.js
@@ -3,7 +3,7 @@ import React from "react";
 const Welcome = () => {
   return (
     <div>
-      <h1>Welcome to Vacation Planner.</h1>
+      <h1>Welcome to Vacation Planner!</h1>
     </div>
   );
 };
