76f6006c263b406e445bcb58b86719e8bc5e564d
Kenya-a <Kenya-a@users.noreply.github.com>
2019-08-27 17:33:37 -0500

    "simple" chat app located in ChatBot folder

--- front-end/src/ChatBot/MessageForm.js
+++ front-end/src/ChatBot/MessageForm.js
@@ -1,33 +1,44 @@
 import React from 'react';
 
-class MessageForm extends React.Component{
+class MessageForm extends React.Component {
     constructor() {
         super();
         this.state = {
-            item: ""
+            //userId: "",
+            item: "",
+            
         };
     }
 
     newSubmit = element => {
         element.preventDefault();
         this.props.addTodo(this.state.item)
+        //this.props.addTodo(this.state.userId)
     }
 
     handleChange = element => {
         this.setState({
-            [element.target.name]:element.target.value
+            [element.target.name]: element.target.value
         });
     };
 
     render() {
-        return(
-            <form onSubmit={this.newSubmit}> 
-                <input 
-                placeholder = 'Enter Message Here'
-                type = 'text'
-                value = {this.state.item}
-                name = 'item'
-                onChange = {this.handleChange}
+        return (
+            <form onSubmit={this.newSubmit}>
+                {/* <input 
+                    placeholder = 'Name'
+                    type = 'text'
+                    value = {this.state.userId}
+                    name = 'username'
+                    onChange = {this.handleChange}
+                /> */}
+
+                <input
+                    placeholder='Enter Message Here'
+                    type='text'
+                    value={this.state.item}
+                    name='item'
+                    onChange={this.handleChange}
                 />
                 <button>Send</button>
             </form>
--- front-end/src/ChatBot/MessageList.js
+++ front-end/src/ChatBot/MessageList.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import Message from './Message';
 
+const data = [
+    {
+        senderId: "Julia",
+        text: 'Hey, how about Tokyo?'
+    },
+
+    {
+        senderId: "Thiery",
+        text: '... how about going home 😊.'
+    },
+
+    {
+        senderId: "Julia",
+        text: '🤦‍.'
+    }
+]
+
+
 const MessageList = props => {
 
     return (
         <div className="todo-List">
+            {data.map((message, index) => {
+                    return (
+                        <>
+                        <div>{message.senderId}</div>
+                        <div>{message.text}</div>
+                        <br/>
+                        </>
+                    )
+                })}
             {props.todos.map(item => (
                 <Message key={item.id} item={item} toggleItem={props.toggleItem} />
             ))}
