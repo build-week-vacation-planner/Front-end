5580c0834ddb89ebbcf3526930b70f4c7085d2a5
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-08-27 15:11:42 -0700

    working on connected back end data to TripList component

--- front-end/src/components/TripList.js
+++ front-end/src/components/TripList.js
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Trip from "./Trip";
 import { Link } from "react-router-dom";
 import Header from "./Header";
+import axios from "axios";
 
 //add data regarding existing Trips
+// const tripData = [
+//   {
+//     id: 1,
+//     destination: "NYC",
+//     startDate: "May 2",
+//     endDate: "May 14",
+//     places: ["Statue of Liberty", "Empire State Building", "Central Park"]
+//   },
+//   {
+//     id: 2,
+//     destination: "Paris",
+//     startDate: "June 10",
+//     endDate: "June 25",
+//     places: ["Eiffel Tower", "Louvre", "Versailles"]
+//   }
+// ];
 
 const TripList = () => {
-  const tripData = [
-    {
-      id: 1,
-      destination: "NYC",
-      startDate: "May 2",
-      endDate: "May 14",
-      places: ["Statue of Liberty", "Empire State Building", "Central Park"]
-    },
-    {
-      id: 2,
-      destination: "Paris",
-      startDate: "June 10",
-      endDate: "June 25",
-      places: ["Eiffel Tower", "Louvre", "Versailles"]
-    }
-  ];
+  const [trip, setTrip] = useState([]);
+
+  const getTrips = id => {
+    axios
+      .get(`https://build-week-vacationplanner.herokuapp.com/vacation/${id}`)
+      .then(res => {
+        console.log(res);
+        setTrip(res.data);
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  useEffect(() => {
+    getTrips();
+  }, []);
 
   return (
     <div>
       <Header />
       <h2>Existing Trips:</h2>
-      tripData.map(trip =>
-      {
-        <Link to={`/trip/${id}`}>
+      {trip.map(trip => {
+        return (
           <div className="trip-preview">
             <h2>{trip.destination}</h2>
             <p>
@@ -37,10 +52,9 @@ const TripList = () => {
             </p>
             <span>"Arrow Icon"</span>
           </div>
-        </Link>
-      }
-      )
-      <Trip tripData={tripData} />
+        );
+      })}
+      <Trip trip={trip} />
       <br />
       <Link to="/trip-form">
         <button>Create a New Trip</button>
