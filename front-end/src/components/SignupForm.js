0d08a00974d01eb8e48722d990daf1e3f03b8963
LC Carrier <lc@LCs-MacBook-Pro.local>
2019-08-29 13:08:49 -0400

    updated login and signup forms

--- front-end/src/components/SignupForm.js
+++ front-end/src/components/SignupForm.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
+import styled from 'styled-components'
+import { Link } from "react-router-dom";
 
 const SignupForm = ({ values, errors, touched, handleSubmit, status }) => {
   const [userList, setUserList] = useState([]);
@@ -14,13 +16,28 @@ const SignupForm = ({ values, errors, touched, handleSubmit, status }) => {
 
   return (
     <div>
-      <h1>Sign Up Here</h1>
+      <StyledDiv>
+      <StyledWrapper>
+      <StyledHeader>Sign Up</StyledHeader>
       <Form>
-        <Field type="text" name="username" placeholder="Username" />
-        <Field type="password" name="password" placeholder="Password" />
 
-        <button type="submit">Submit</button>
+        <StyledGroup>
+          <StyledLabel>Username</StyledLabel>
+          <StyledField type="text" name="username" placeholder="Username" />
+        </StyledGroup>
 
+        <StyledGroup>
+        <StyledLabel>Password</StyledLabel>
+        <StyledField type="password" name="password" placeholder="Password" />
+        </StyledGroup>
+
+        <StyledButton type="submit">Get started</StyledButton>
+        <StyledP>Already have an account? 
+          
+          <Link to="/login">
+          <StyledA href="#">Login Up</StyledA>
+          </Link>
+        </StyledP>
         <div>
           {touched.username && errors.username && (
             <p className="error">{errors.username}</p>
@@ -32,6 +49,8 @@ const SignupForm = ({ values, errors, touched, handleSubmit, status }) => {
           )}
         </div>
       </Form>
+      </StyledWrapper>
+      </StyledDiv>
     </div>
   );
 };
@@ -72,5 +91,98 @@ const FormikSignupForm = withFormik({
     }
   }
 })(SignupForm);
+
+
+// Styled components
+const StyledField = styled(Field)`
+    width: 600px;
+    height: 48px;
+    border-radius: 3px;
+    border: 1px solid #EBEBEB;
+    box-sizing: border-box;
+    font-size: 16px;
+    line-height: 21px;
+    padding-left: 12px;
+    margin-bottom: 19px;
+
+    :focus {
+      border: 2px solid #008489;
+      outline: none;
+      padding-left: 11px;
+
+    :placeholder {
+      color: #484848;
+    }
+    }
+`
+
+const StyledGroup = styled.div`
+  display: flex; 
+  flex-direction: column;
+`
+
+const StyledLabel = styled.label`
+  font-size: 12px;
+  line-height: 16px;
+  color: #484848;
+  margin-bottom: 8px;
+  text-transform: uppercase;
+  font-weight: 700;
+`
+
+const StyledButton = styled.button`
+  background-color: #FE5B5E;
+  color: white;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 700;
+  border: none;
+  padding: 12px 25px; 
+  border-radius: 4px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  font-family: 'Cereal';
+
+  :hover {
+  transition: all 0.3s cubic-bezier(0.785, 0.135, 0.15, 0.86) 0s;
+  cursor: pointer;
+  }
+`
+
+const StyledHeader = styled.h1`
+  font-size: 24px 
+  color: #484848;
+  line-height: 30px; 
+  margin-bottom: 30px;
+  font-weight: 900;
+`
+
+const StyledWrap = styled.div`
+  margin-top: 50px;
+`
+
+const StyledP = styled.p`
+  color:  #58626D;
+  margin-bottom: 20px;
+`
+
+const StyledA = styled.a`
+  color: #008489;
+  text-decoration: underline;
+  margin-left: 5px;
+  font-weight: 600;
+`
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+
+`
+
+const StyledWrapper = styled.div`
+  display: inline-block;
+  width: 600px;
+`
 
 export default FormikSignupForm;
